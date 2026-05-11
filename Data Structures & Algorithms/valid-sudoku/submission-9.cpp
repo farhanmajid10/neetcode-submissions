d30ce6f33bc722723c890cb4cd5ab89cefc2a470
Farhan Majid <farhanmajid23@gmail.com>
2026-05-11 13:09:14 -0500

    Add: valid-sudoku - submission-9

---
+++ Data Structures & Algorithms/valid-sudoku/submission-9.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_map<int,vector<int>> rows;
+        unordered_map<int,vector<int>> cols;
+        unordered_map<int,vector<int>> boxes;
+
+        for(int i = 0; i < 9;i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] == '.'){
+                    
+                }else{
+                    int temp = board[i][j] - '0';
+                    if(find(rows[i].begin(),rows[i].end(), temp) == rows[i].end()){
+                        rows[i].push_back(temp);
+                    }else{
+                        return false;
+                    }
+                    if(find(cols[j].begin(),cols[j].end(), temp) == cols[j].end()){
+                        cols[j].push_back(temp);
+                    }else{
+                        return false;
+                    }
+                    int box = ((3*(i/3)) + j/3);
+                    if(find(boxes[box].begin(), boxes[box].end(), temp) == boxes[box].end()){
+                        boxes[box].push_back(temp);
+                    }else{return false;}
+                }
+            }
+        }
+        
+        return true;
+    }
+};
