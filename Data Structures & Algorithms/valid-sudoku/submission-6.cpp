4616049d8dac00951aa87856e28c15b137c46fff
Farhan Majid <farhanmajid23@gmail.com>
2026-05-11 13:05:32 -0500

    Add: valid-sudoku - submission-6

---
+++ Data Structures & Algorithms/valid-sudoku/submission-6.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_map<int,vector<int>> rows;
+        unordered_map<int,vector<int>> cols;
+        unordered_map<int,vector<int>> boxes;
+
+        for(int i = 0; i < 9;i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] == '.'){
+                    
+                }else{
+                    int temp = board[i][j] - '0';
+                    if(find(rows[i].begin(),rows[i].end(), temp) == rows[i].end()){
+                        rows[i].push_back(temp);
+                    }else{
+                        return false;
+                    }
+                }
+                if(board[j][i] == '.'){
+                    
+                }else{
+                    int temp = board[j][i] - '0';
+                    if(find(cols[i].begin(),cols[i].end(), temp) == cols[i].end()){
+                        cols[i].push_back(temp);
+                    }else{
+                        return false;
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                int box = ((3*(i/3)) + j/3);
+                if(board[i][j] == '.'){
+                    
+                }else{
+                    int temp = board[i][j] - '0';
+                    if(find(boxes[box].begin(), boxes[box].end(), temp) == boxes[box].end()){
+                        boxes[box].push_back(temp);
+                    }else{return false;}
+                }
+            }
+        }
+        return true;
+    }
+};
