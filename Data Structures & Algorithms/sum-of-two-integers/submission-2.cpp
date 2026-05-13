10eb31ae15fdd3624ce641fe78a36f7cb6351364
Farhan Majid <farhanmajid23@gmail.com>
2026-05-12 20:57:37 -0500

    Add: sum-of-two-integers - submission-2

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        int result = 0;
+        int carry = 0;
+        for(int i = 0; i < 32; i++){
+            int a_bit = (a & 1);
+            int b_bit = (b & 1);
+            int sum = (a_bit ^ b_bit ^ carry);
+            carry = ((a_bit & b_bit)|(a_bit & carry)|(b_bit & carry)); 
+            a >>= 1;
+            b >>= 1;
+            sum <<= i;
+            result += sum;
+        }
+        return result;
+    }
+};
